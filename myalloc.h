97fc766dde6eaa024a9ad8dc60b41d5820f943eb
Bipoc <pinonbrieuc@gmail.com>
2016-03-10 18:05:36 +0100

    update with myalloc

--- myalloc.h
+++ myalloc.h
@@ -19,9 +19,17 @@ void* mymalloc(size_t size);
 void* mycalloc(size_t size);
 void myfree(void* ptr);
 
-void dropHeapLimit(void* ptrLastFreedBlock);
-
 void initHeapLimitAtLaunch();
+
+/**
+allocate the block at the address passed in argument for the size
+passed in argument
+if the size does not fit perfectly in the block we create a new block
+in the rest of the free memory (at the end)
+the size is supposed to be <= than the size of the block
+note : this work only if HEADER_SIZE == BYTES_ALIGNEMENT
+**/
+void allocateBlock(void* blockAddress, size_t size);
 
 /**
 return the address of the block's header that best fit in memory
