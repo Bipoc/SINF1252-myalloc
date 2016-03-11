d92837156691e1680af17f6093dcf2fd1dafc889
Bipoc <pinonbrieuc@gmail.com>
2016-03-11 20:48:16 +0100

    correct error and add printf
    error is (with block a pointer) (void*)block+something instead of block+something

--- myalloc.c
+++ myalloc.c
@@ -8,31 +8,39 @@ extern size_t memSize;
 
 void* mymalloc(size_t size)
 {
+	if (debugLevel == 2)
+		printf("\tenter mymalloc with arg : %d\n", size);
 	initHeapLimitAtLaunch();
 
 	if (size <=0)
 		return NULL;
 
+	if (debugLevel == 2)
+		printf("\tmymalloc 2\n");
+	
 	size_t nbBytes = size/4;
 	if (size%4 > 0)
 		nbBytes++;
 	nbBytes *= 4;
 
 	if (debugLevel == 2)
-		printf("\t[[[ mymalloc==> Allocates %d bytes (+ %d header)...\n", (int) nbBytes, HEADER_SIZE);
+		printf("\t[[[ mymalloc==> Allocates %d bytes (+ %d header)...\n", (int) nbBytes, HEADER_SIZE);//bytes or bits
 
-
+	if (debugLevel == 2)
+		printf("\tenter Best fit : %d\n", nbBytes);
 	void* blockAddress = bestFit(nbBytes);
 
-	if ((long) blockAddress == -1)
-		return NULL;
+	if (debugLevel == 2)
+		printf("\tpass Best fit\n");
 
 	if (blockAddress == NULL)
 		return NULL;
 
+	if (debugLevel == 2)
+		printf("\tenter allocateBlock\n");
 	allocateBlock(blockAddress, size);
 
-	return blockAddress + HEADER_SIZE;
+	return (void*)blockAddress + HEADER_SIZE;
 }
 
 void* mycalloc(size_t size)
@@ -40,8 +48,8 @@ void* mycalloc(size_t size)
 	short* address = mymalloc(size);//sizeof short always ==1?
 	if (address == NULL)
 		return NULL;
-
-	for (int i=0; i<size; i++)
+	int i;
+	for (i=0; i<size; i++)
 		*(address+i) = 0;
 
 	return address;
@@ -52,13 +60,13 @@ void myfree(void* ptr)
 	if (ptr == NULL)
 		return;
 
-	isReleasable(ptr);
+	isBlock(ptr);
 
 	block_header* ptrHeader = (block_header*) (ptr-HEADER_SIZE);
 	ptrHeader->alloc = 0;
 }
 
-void isReleasable(void* ptr)
+void isBlock(void* ptr)
 {
 	if (ptr == NULL)
 		exit(EXIT_FAILURE);//TODO change?
@@ -103,15 +111,32 @@ void allocateBlock(void* blockAddress, size_t size)
 
 void initHeapLimitAtLaunch()
 {
+	if (debugLevel == 2)
+		printf("\tenter initHeapLimitAtLaunch\n");
+
 	static int isInit = 0;
 	if (!isInit)
 	{
 		heapLimitAtLaunch = sbrk(0);
+
+		size_t temporary = memSize/4;
+		if (memSize%4 > 0)
+			temporary++;
+		memSize = temporary * 4;
+
 		void *answer = sbrk(memSize);
 		if ((long)answer == -1)
 			exit(EXIT_FAILURE);//TODO add message with errno
-		
+
 		actualHeapLimit = heapLimitAtLaunch + memSize;
+
+		block_header* firstBlock = (block_header*)heapLimitAtLaunch;
+		firstBlock->alloc = 0;
+		firstBlock->zero = 0;
+		firstBlock->size = memSize - HEADER_SIZE;
+
+		if (debugLevel>=2)
+			printf("%p, %p\n", heapLimitAtLaunch, actualHeapLimit);
 
 		isInit = 1;
 	}
@@ -122,15 +147,28 @@ void* bestFit(size_t nbBytes)
 	void* currentAddress = heapLimitAtLaunch;
 	block_header* currentBlock;
 
+	if (debugLevel >= 2)
+		printf("\tbestFit 1\n");
+
 	void* bestFitAddress=NULL;
 	size_t sizeBestFit;
 
+	if (debugLevel >= 2)
+		printf("\t\t 2\n");
+
 	while (currentAddress < actualHeapLimit)
 	{
-
+		if (debugLevel >= 2)
+		printf("\t\t 3\n");
 		currentBlock = (block_header*)currentAddress;
 
+		if (debugLevel >= 2)
+		printf("\t\t 4\n");
+
 		updateBlock(currentBlock);
+
+		if (debugLevel >= 2)
+		printf("\t\t 5\n");
 
 		if (currentBlock->alloc == 0)
 		{
@@ -141,27 +179,55 @@ void* bestFit(size_t nbBytes)
 			}
 		}
 
+		if (debugLevel >= 2)
+		printf("\t\t 6\n");
 		currentAddress += currentBlock->size + HEADER_SIZE;
 	}
+
+	if (debugLevel >= 2)
+		printf("\t\t 7\n");
 
 	return bestFitAddress;
 }
 
 void updateBlock(block_header* block)
 {
+	if (debugLevel >= 2)
+		printf("\t\t enter updateblock\n");
+
 	if (block == NULL || block->alloc == 1)
 		return;
 
+	if (debugLevel >= 2)
+		printf("\t\t\t 1\n");
+
 	size_t totalSize = block->size;
 
-	void* currentAddress = &block + HEADER_SIZE + block->size;
+	if (debugLevel >= 2)
+		printf("\t\t\t 2\n");
+
+	void* currentAddress = (void*)block + HEADER_SIZE + block->size;
 	block_header* currentBlock = (block_header*)currentAddress;
-	while (currentBlock->alloc == 0)
+	if (debugLevel >= 2)
+		printf("\t\t\t 3\n");
+
+	while (currentBlock->alloc == 0 && currentAddress < actualHeapLimit)
 	{
+		if (debugLevel >= 2)
+		printf("\t\t\t 4\n");
 		totalSize += HEADER_SIZE + currentBlock->size;
+		if (debugLevel >= 2)
+		printf("\t\t\t 4a\n");
 		currentAddress += HEADER_SIZE + currentBlock->size;
+		if (debugLevel >= 2)
+		printf("\t\t\t 4b\n");
 		currentBlock = (block_header*)currentAddress;
+		if (debugLevel >= 2)
+		printf("\t\t\t 4c\n");
 	}
+
+	if (debugLevel >= 2)
+		printf("\t\t\t 6\n");
 
 	block->size = totalSize;
 }
