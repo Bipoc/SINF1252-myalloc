de0d1085fdcd3701c7c421ce59b60b2f03ebeec3
Bipoc <pinonbrieuc@gmail.com>
2016-03-12 18:48:48 +0100

    setUp working

--- main.c
+++ main.c
@@ -11,27 +11,44 @@ size_t memSize;
 
 int main(int argc, char *argv[])
 {
+	/**
 	if (argc<1)
 		exit(EXIT_FAILURE);
 
 	memSize = (size_t)strtol(argv[1], NULL, 10);
+	
+	printf("%d\n", memSize);
+	**/
 
-	//printf("%d\n", memSize);
+
+	setHeapLimitAtLaunch();
+
+	memSize = MEM_SIZE_TESTS;
+
+	mymalloc(0);
 
 
 	if (CUE_SUCCESS != CU_initialize_registry())
 		return CU_get_error();
 	
-	CU_pSuite pSuite = NULL;	
 
-	pSuite = CU_add_suite("my_suite", initTests , finishTests);
-	if (NULL == pSuite)
+
+	//other tests suites
+
+
+
+
+	CU_pSuite pSuiteCalloc = NULL;	
+
+	pSuiteCalloc = CU_add_suite("calloc_suite", initCallocTests , finishCallocTests);
+	if (NULL == pSuiteCalloc)
 	{
 		CU_cleanup_registry();
 		return CU_get_error();
 	}
 
-	if (NULL == CU_add_test(pSuite, "Test memory as good size", checkSize))
+	if (NULL == CU_add_test(pSuiteCalloc, "Test memory as good size", checkSize)
+		|| NULL == CU_add_test(pSuiteCalloc, "Test memory is well set at zero by calloc", checkCalloc))
 	{
 		CU_cleanup_registry();
 		return CU_get_error();
@@ -41,6 +58,8 @@ int main(int argc, char *argv[])
 	CU_basic_show_failures(CU_get_failure_list());
 
 	CU_cleanup_registry();
+
+	printf("\n");
 
 	return EXIT_SUCCESS;
 }
