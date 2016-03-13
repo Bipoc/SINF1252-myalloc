c708f89c8329dfdb69b99e5ac019270250375d03
Bipoc <pinonbrieuc@gmail.com>
2016-03-13 14:17:45 +0100

    regular update

--- tests.h
+++ tests.h
@@ -1,31 +1,53 @@
 #ifndef _TESTS_H_
 #define _TESTS_H_
 
-#include <stdio.h>
-#include <stdlib.h>
-#include <unistd.h>
 #include <CUnit/CUnit.h>
+void CU_basic_run_tests();//to avoid warning
+void CU_basic_show_failures();
 
 #define MEM_SIZE_TESTS 40
 
+//start
+//only one call and before any call to mymalloc or my calloc
 void setHeapLimitAtLaunch();
 
-void printMem();
+//launch CUnit Tests
+int cunitTests();
 
+//print
+void printMem();
 void printBytes();
 
-void checkSize();
-
-void checkCalloc();
-
-int isFullOfZeros(void* address, size_t size);
-
+//tools
 void resetMem();
-
 void fullBlockWithOnes(void* addressBlock);
 
+//basic tests : first tests with basic call
+void testBasicFree();
+void testBasicAllocAndFree();
+void testBlockbyBlockBasic();
+void mustFail();
+
+//Free tests
+void testBlockbyBlockFree();
+void allocMustSuccess();
+
+//Limits tests : test at the start and the end of memory
+void testBlockbyBlockLimits();
+void allocMustFail();
+
+//calloc tests
+void checkSize();
+void checkCalloc();
+int isFullOfZeros(void* address, size_t size);
+
+
+//tests initiater/finisher
+int initBasicTests();
+int initFreeTests();
+int initLimitsTests();
 int initCallocTests();
 
-int finishCallocTests();
+int finishTests();
 
 #endif
