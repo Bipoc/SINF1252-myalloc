bf855e67270a40be6cd9f0f144c55308d7d885f3
Bipoc <pinonbrieuc@gmail.com>
2016-03-13 14:16:47 +0100

    add/correct comment

--- myalloc.h
+++ myalloc.h
@@ -1,8 +1,8 @@
 #ifndef _MYALLOC_H
 #define _MYALLOC_H_
 
-#include <stdlib.h>
 #include <stdio.h>
+#include <stdlib.h>
 #include <unistd.h>
 
 #define HEADER_SIZE		4
@@ -16,24 +16,27 @@ typedef struct block_header
 }block_header;
 
 void* mymalloc(size_t size);
-void* mycalloc(size_t size);
+void* mycalloc(size_t nmemb, size_t size);
 void myfree(void* ptr);
 
 /**
 test if an address is an effective block
-if not throw an error
 **/
-void isBlock(void* ptr);
+int isBlock(void* ptr);
 
+/**
+initialize the heap on his first launch, by calling sbrk
+if memSize is negative or sbrk return -1, it stop the program 
+**/
 void initHeapLimitAtLaunch();
 
 /**
 allocate the block at the address passed in argument for the size
 passed in argument
-if the size does not fit perfectly in the block we create a new block
+if the size does not fit perfectly in the block we create a new free block
 in the rest of the free memory (at the end)
 the size is supposed to be <= than the size of the block
-note : this work only if HEADER_SIZE == BYTES_ALIGNEMENT
+note : the behavior is not guaranted if HEADER_SIZE != BYTES_ALIGNEMENT
 **/
 void allocateBlock(void* blockAddress, size_t size);
 
@@ -41,15 +44,14 @@ void allocateBlock(void* blockAddress, size_t size);
 return the address of the block's header that best fit in memory
 nbBytes is supposed to be >0
 if NULL is returned, none address is convenient
-if -1 is returned, sbrk fail to give the heapLimit (possible change of that behavior)
 !it doesn't allocate the block it just give is position!
-group sequence of free block and reduce heaplimit if possible (possible change of that behavior)
+group sequence of free block
 **/
 void* bestFit(size_t nbBytes);
 
 /**
 increase the size of the block if possible
-	if the block is followed by other free block(s) and is itself free
+	-> if the block is followed by other free block(s) and is itself free
 **/
 void updateBlock(block_header* block);
 
