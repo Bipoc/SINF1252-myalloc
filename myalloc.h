0978a59694151bef6ff37f0ac62a69d931b3b4fd
sigustin <simon.gustin@student.uclouvain.be>
2016-03-15 17:28:17 +0100

    Comments corrections

--- myalloc.h
+++ myalloc.h
@@ -21,38 +21,38 @@ void* mycalloc(size_t nmemb, size_t size);
 void myfree(void* ptr);
 
 /**
-test if an address is an effective block
+tests if the address pointed by the pointer is an effective block
 **/
 int isBlock(void* ptr);
 
 /**
-initialize the heap on his first launch, by calling sbrk
+initializes the heap on the first call to this function, by calling sbrk
 if memSize is negative or sbrk return -1, it stop the program 
 **/
 void initHeapLimitAtLaunch();
 
 /**
-allocate the block at the address passed in argument for the size
-passed in argument
-if the size does not fit perfectly in the block we create a new free block
+allocates the block at the address given as a parameter for the size
+given as a parameter
+if the size does not fit perfectly the current block, we create a new free block
 in the rest of the free memory (at the end)
-the size is supposed to be <= than the size of the block
-note : the behavior is not guaranted if HEADER_SIZE != BYTES_ALIGNEMENT
+the size is supposed to be greater or equal to the size of the block
+note : the behavior is not guaranteed if HEADER_SIZE != BYTES_ALIGNEMENT
 **/
 void allocateBlock(void* blockAddress, size_t size);
 
 /**
-return the address of the block's header that best fit in memory
-nbBytes is supposed to be >0
-if NULL is returned, none address is convenient
-!it doesn't allocate the block it just give is position!
-group sequence of free block
+returns the address of the block's header that best fit in memory
+nbBytes is supposed to be greater than 0
+if NULL is returned, there isn't any convenient address available
+!it doesn't allocate the block it just give its position!
+the function also groups following sequences of free block
 **/
 void* bestFit(size_t nbBytes);
 
 /**
-increase the size of the block if possible
-	-> if the block is followed by other free block(s) and is itself free
+increases the size of the block if the block
+is followed by other free block(s) and is itself free
 **/
 void updateBlock(block_header* block);
 
